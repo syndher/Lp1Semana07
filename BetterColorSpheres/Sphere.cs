f3f1beed8d79158ceca0fd0bf0e7228352a0a496
Syndher <miguelfilipe.dev@gmail.com>
2025-04-03 21:18:50 +0100

    Sphere.cs changes

--- BetterColorSpheres/Sphere.cs
+++ BetterColorSpheres/Sphere.cs
@@ -29,9 +29,7 @@ namespace ColorSpheres
             }
 
         }
-        public int GetTimesThrown()
-        {
-            return _bounces;
-        }
+        public int GetTimesThrown => _bounces;
+
     }   
 }
