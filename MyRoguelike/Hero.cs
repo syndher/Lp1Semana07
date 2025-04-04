af915c0bab965cdffa8dd26b974029cd22be58f3
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-04-04 17:18:00 +0100

    Add xp and hp

---
+++ MyRoguelike/Hero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml.XPath;
+
+namespace MyRoguelike
+{
+
+    public class Hero
+    {
+        
+            private int _xp;
+            private float _health;
+            public string GetName => _name;
+            public int Level => 1 + _xp / 1000;
+
+        public Hero(int xp)
+        {
+                _xp = xp;
+                _health = _health;
+                
+
+        }
+        public int XP(int xp)
+        {
+            if (xp > 0)
+            {
+                _xp += xp;
+            }
+            return _xp;
+
+        }
+    }
+}
--- MyRoguelike/Program.cs
+++ MyRoguelike/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyRoguelike
+{
+
+    class Program
+    {
+
+        private static void Main(string[] args)
+        {   
+
+        }
+    }
+}
